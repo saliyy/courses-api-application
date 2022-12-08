90ad79f736f762e849d2ef0a59ac0b4a4d511e0b
saliy <saliyvls@yandex.ru>
2022-12-08 15:57:55 +0300

    dsad

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -11,8 +11,8 @@ export class CreateReviewDto {
 	@IsString()
 	description: string;
 
-	@Min(1, { message: 'Рэйтинг не может быть меньше 1' })
-	@Max(5, { message: 'Рэйтинг не может быть больше 5' })
+	@Min(1, { message: 'Рейтинг не может быть меньше 1' })
+	@Max(5, { message: 'Рейтинг не может быть больше 5' })
 	@IsNumber()
 	rating: number;
 
